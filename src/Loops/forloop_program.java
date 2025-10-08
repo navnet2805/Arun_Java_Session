4d00a982afc21e4b5c359e5c4e5c349094b0ecb9
Naveen <naveen.rp.2805@gmail.com>
2025-10-08 11:03:59 +0530

    03

---
+++ src/Loops/forloop_program.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class forloop_program {
+    public static void main(String[] args) {
+
+
+        for (int i = 1 ;i<=10; i++)
+        {
+            System.out.println("10*"+i+"="+10*i);
+        }
+    }
+}
